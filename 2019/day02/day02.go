7db6feb27672b96e72b132afcbcdba3257f4d6ca
Ryan Yeske <ryan@ryanyeske.com>
2019-12-01 23:21:45 -0700

    part 2

--- 2019/day02/day02.go
+++ 2019/day02/day02.go
@@ -31,19 +31,31 @@ func main() {
 		program = append(program, j)
 	}
 	
+	// part 1
+	fmt.Println(process(program, 12, 2))
 
-	process(program)
 
-	
+	// part 2
+	for noun := 0; noun < 99; noun++ {
+		for verb := 0; verb < 99; verb++ {
+			result := process(program, noun, verb)
+			if (result == 19690720) {
+				fmt.Println(100 * noun + verb)
+				goto done
+			}
+		}
+	}
+	done:
 }
 
-func process(program []int) {
-//	fmt.Printf("program: %d", program)
-
+func process(prog []int, noun int, verb int) int {
 	pc := 0
 
-	program[1] = 12
-	program[2] = 2
+	program := make([]int, len(prog))
+	copy(program, prog)
+
+	program[1] = noun
+	program[2] = verb
 
 	for program[pc] != 99 {
 		//fmt.Printf("program = %d, pc=%d\n", program, pc)
@@ -59,5 +71,6 @@ func process(program []int) {
 		}
 		pc = pc + 4
 	}
-	fmt.Println(program)
+
+	return program[0]
 }
